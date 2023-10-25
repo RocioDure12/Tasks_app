b4bc3ec61653df2bd484d9372cffdce1652d2f90
Rocio Dure <rocioevelyndure@gmail.com>
2023-10-25 13:17:07 -0300

    funcion create

--- src/modules/repositories/tasks_repository.py
+++ src/modules/repositories/tasks_repository.py
@@ -1,7 +1,21 @@
+from sqlmodel import Session
+from ..models.task import Task
+from ..services.db_services import DbServices
 #crud
 
 class TasksRepository():
-    def create():
-        pass
+    def __init__(self):
+        self._db_services=DbServices()
+    
+    def create(self, item:Task):
+        with Session(self._db_services.get_engine()) as session:
+            session.add(item)
+            session.commit()
+            session.refresh(item)
+        
+        return item
+    
+        
+        
     
 
