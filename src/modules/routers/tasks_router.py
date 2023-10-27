6c3c80e790868856780f23573a81d02802c5c1b8
Rocio Dure <rocioevelyndure@gmail.com>
2023-10-27 10:44:47 -0300

    task router

---
+++ src/modules/routers/tasks_router.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter, Response
+from ..controllers.tasks_controller import TasksController
+from ..models.task import Task
+from typing import List
+
+router = APIRouter(
+    prefix='/tasks'
+)
+
+controller = TasksController()
+
+router.add_api_route('/', controller.create, methods=['GET'])
