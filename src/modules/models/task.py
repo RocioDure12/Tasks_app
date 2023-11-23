045f48552e945b80a9624c93aefa1c70ce33b8e8
Rocio Dure <rocioevelyndure@gmail.com>
2023-11-23 10:50:46 -0300

    añado atributos a model task

--- src/modules/models/task.py
+++ src/modules/models/task.py
@@ -1,5 +1,7 @@
 from typing import Optional
-from sqlmodel import Field,Relationship,SQLModel
+from sqlmodel import Field,Relationship,SQLModel,Column, Date
+from sqlalchemy import BigInteger, Integer
+from datetime import datetime
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from ..models.user import User
@@ -9,5 +11,8 @@ class Task(SQLModel, table=True):
     task_name:str
     description:str
     status:bool
+    deleted_at:Optional[datetime] = None
+    created_at:Optional[datetime] = Field(default_factory=datetime.utcnow,nullable=False)
+    updated_at:Optional[datetime] = None
     user_id:Optional[int]=Field(default=None, foreign_key="user.id")
     user:Optional["User"]=Relationship(back_populates="tasks")
