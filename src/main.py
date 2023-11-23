bdd6b15c4f758965fb0a5b6b71eb0428beb6d8c9
Rocio Dure <rocioevelyndure@gmail.com>
2023-11-23 12:25:45 -0300

    cambios en main

--- src/main.py
+++ src/main.py
@@ -3,11 +3,18 @@ load_dotenv()
 from fastapi import FastAPI
 from modules.routers.tasks_router import router as tasks_router
 from modules.routers.users_router import router as users_router
+from modules.services.db_services import DbServices
+from sqlmodel import Field, SQLModel, create_engine 
 
 app=FastAPI()
+db=DbServices()
 
 app.include_router(tasks_router)
 app.include_router(users_router)
 
+"""
+def create_tables():
+    SQLModel.metadata.create_all(db.get_engine()) 
 
-    
+create_tables()
+"""
